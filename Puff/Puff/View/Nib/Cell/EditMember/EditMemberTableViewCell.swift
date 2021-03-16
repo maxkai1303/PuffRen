10a993a421670226f4c09bb3266323d6914b7040
Max <k1071303@gmail.com>
2021-03-16 15:02:02 +0800

    Create Edit Member Xib.

---
+++ Puff/Puff/View/Nib/Cell/EditMember/EditMemberTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EditMemberTableViewCell.swift
+//  Puff
+//
+//  Created by Max Kai on 2021/3/16.
+//
+
+import UIKit
+
+class EditMemberTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
